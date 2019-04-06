8b9c6550dab68cee6d31e2827999b0ca2d8f9630
SzilardZ <szilard.zr@gmail.com>
2019-04-06 18:00:40 +0200

    Initial commit: base spring boot structure

---
+++ src/main/java/com/szilardz/travelbucketlist/TravelBucketlistApplication.java
@@ -0,0 +1,13 @@
+package com.szilardz.travelbucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBucketlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBucketlistApplication.class, args);
+    }
+
+}
