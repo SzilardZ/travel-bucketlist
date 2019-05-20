5bf05ce0eec5309024c115ed6bce27a802bfa3e3
SzilardZ <szilard.zr@gmail.com>
2019-05-20 09:35:10 +0200

    added two method to user repository

--- src/main/java/com/szilardz/travelbucketlist/repository/UserRepository.java
+++ src/main/java/com/szilardz/travelbucketlist/repository/UserRepository.java
@@ -11,5 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User getById(Long id);
     Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 
 }
