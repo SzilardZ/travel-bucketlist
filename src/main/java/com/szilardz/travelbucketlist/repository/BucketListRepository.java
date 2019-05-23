c76b0a7d6923ad29ec705a00e4d5bd233c9a8634
SzilardZ <szilard.zr@gmail.com>
2019-05-23 15:24:37 +0200

    add new methods to bucketlist repository and user repository

--- src/main/java/com/szilardz/travelbucketlist/repository/BucketListRepository.java
+++ src/main/java/com/szilardz/travelbucketlist/repository/BucketListRepository.java
@@ -1,6 +1,7 @@
 package com.szilardz.travelbucketlist.repository;
 
 import com.szilardz.travelbucketlist.model.BucketList;
+import com.szilardz.travelbucketlist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,8 @@ public interface BucketListRepository extends JpaRepository<BucketList, Long> {
     BucketList getByBucketListId(Long id);
 
     BucketList getBucketListByUserId(Long id);
+
+    BucketList getBucketListByUser(User user);
+
+
 }
